f8ae10c4affb320cf280fead2ae8b01533f98a26
CaLLab <benmorris@uchicago.edu>
2017-06-29 16:14:47 -0500

    restoring carousel nav

--- js/index.js
+++ js/index.js
@@ -9,16 +9,19 @@ function ageMe(person) {
 //smooth scrolling function
 $(function() {
        $("a[href*='#']:not([href='#'])").click(function() {
-           if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-               var target = $(this.hash);
-               target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-               if (target.length) {
-                   $('html, body').animate({
-                       scrollTop: target.offset().top-35
-                   }, 600);
-                   return false;
-               }
-           }
+          console.log(this.href)
+          if(!this.href.includes('carousel')) {
+             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                 var target = $(this.hash);
+                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                 if (target.length) {
+                     $('html, body').animate({
+                         scrollTop: target.offset().top-35
+                     }, 600);
+                     return false;
+                 }
+             }
+          }
        });
    })
 
